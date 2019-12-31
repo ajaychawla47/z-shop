38cdf06e636fc1119b601ae0273bfc136253a53c
Ajay Chawla <ajaxchawla47@gmail.com>
2019-12-31 09:31:16 +0530

    my first commit

---
+++ myproject/src/main/java/myproject/MyClass.java
@@ -0,0 +1,18 @@
+package myproject;
+
+public class MyClass {
+
+	 int a;
+	 int b;
+	
+	 int add(int a,int b) {
+		return a+b; 
+	 }
+	 
+	 public static void main(String[]args) {
+		 MyClass mc = new MyClass();
+		 mc.add(10, 20);
+		 
+	 }
+	
+}
